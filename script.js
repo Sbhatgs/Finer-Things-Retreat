b52ef4f902d25451962a65d942cabcba9ad48462
Shashank <sbhatgs@gmail.com>
2025-01-27 22:35:30 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your inquiry! We will get back to you soon.');
+  });
+  
